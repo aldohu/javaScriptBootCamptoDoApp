d47df26a314073958709aa8546ac736198faddbe
Aldin <alhuric89@gmail.com>
2021-11-04 17:56:41 +0100

    add todos to DOM

---
+++ app.js
@@ -0,0 +1,35 @@
+const todos = [
+    {
+        text: "Order cat food",
+        completed: false
+    },
+    {
+        text: "Clean kitchen",
+        completed: true
+    },
+    {
+        text: "Buy food",
+        completed: true
+    },
+    {
+        text: "Do work",
+        completed: false
+    },
+    {
+        text: "Exercise",
+        completed: true
+    }
+];
+
+const leftTodos = todos.filter(e=> !e.completed).length;
+
+let leftTodosParagraph = document.createElement("p");
+leftTodosParagraph.textContent = `You have ${leftTodos} todos left.`
+document.body.append(leftTodosParagraph)
+
+todos.forEach(e=> {
+    let para = document.createElement("p");
+    para.textContent = `${e.text}`;
+    document.body.append(para)
+    
+})
